310564f5d0e8a16dc9f14cd31aebf02207d64dd2
Lisandro Civili <lisandrocivili13@gmail.com>
2024-10-24 15:11:26 -0300

    Primer commit, configurando server

---
+++ server.js
@@ -0,0 +1,11 @@
+console.log("Pasos para crear un server:")
+
+//1- Configurar un puerto (para esto sirve express)
+
+//2- Configurar los middlewares (funciones que van entre las solicitudd y la respuesta HTTP)
+
+//3- COnfigurar rutas
+
+import express from 'express'
+const app = express()
+const port = 3000
